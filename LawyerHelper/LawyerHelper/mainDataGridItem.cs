c68d8180f31e03822da7572b4e4e175efd877683
timbog <timbog@mail.ru>
2014-07-05 17:02:38 +0400

    datagrid added

---
+++ LawyerHelper/LawyerHelper/mainDataGridItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawyerHelper
+{
+    class mainDataGridItem
+    {
+        public float number{get;set;}
+
+        public string aim { get; set; }
+
+        public string planDate { get; set; }
+
+        public int timeFromBegin { get; set; }
+
+        public string factDate { get; set; }
+
+        public int pastDue { get; set; }
+
+        public string comment { get; set; }
+
+        public mainDataGridItem(float number, string aim, string planDate, int timeFromBegin, string factDate, int pastDue, string comment)
+        {
+            this.number = number;
+            this.aim = aim;
+            this.planDate = planDate;
+            this.timeFromBegin = timeFromBegin;
+            this.factDate = factDate;
+            this.pastDue = pastDue;
+            this.comment = comment;
+        }
+    }
+}
